0c644d851fe68a7e5da4206b5a007136ee97c615
Adam Skoczeń <adamsk91@wp.pl>
2024-09-17 21:18:47 +0200

    10 => 30

--- src/components/gallery-api.js
+++ src/components/gallery-api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.unsplash.com/search";
 
 export const fetchGalleryWithTopic = async (searchImages, KEY, currentPageUrl) => {
-  const response = axios.get(`/photos?per_page=10&page=${currentPageUrl}&query=${searchImages}&client_id=${KEY}`)
+  const response = axios.get(`/photos?per_page=30&page=${currentPageUrl}&query=${searchImages}&client_id=${KEY}`)
   return (await response).data.results;
 }
